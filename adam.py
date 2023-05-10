f946176e4cf4f979d70355858e879a613a08d964
adamhammond22 <abhammond22@gmail.com>
2023-05-10 11:38:29 -0700

    making it a class

--- adam.py
+++ adam.py
@@ -7,46 +7,77 @@ import pandas as pd
 import os
 from os.path import exists
 
-root = tk.Tk()
-root.withdraw() #Hide root window
 
-#HARDCODED FILEPATH
+# Class that handles user input
+class inputMassager():
 
-#Filepath Determined by tk dialog
-filepath = filedialog.askopenfilename()
+	def __init__(self):
+
+		self.filepath = ""
+
+	# Opens a file selection dialog with optional message
+	# Returns True on valid filepath selection, False otherwise
+	def askForInput(self, title_msg = "Select Data File"):
+		root = tk.Tk()  #init tkinter root
+		root.withdraw() #Hide root window
+
+		#Filepath Determined by tk dialog
+		self.filepath = filedialog.askopenfilename(title = title_msg)
 
 
-if not (os.path.exists(filepath)):
-	print("Cannot find file at path:", filepath)
-
-
-fLen = len(filepath)
-if (fLen > 4) and (filepath[fLen-4:] == ".txt"):
-	data = open(filepath, "r")    # note mode "r"
-	readingData = False
-	col1 = []
-	col2 = []
-	col3 = []
-	for line in data.readlines():
-		if readingData:
-			data_list = line.split("\t")
-			# print(
-			# 	re.sub("\s", "", data_list[1]),
-			# 	re.sub("\s", "", data_list[2]),
-			# 	re.sub("\s", "", data_list[3]),
-			# 	)
-			col1.append(data_list[1])
-			col2.append(data_list[2])
-			col3.append(data_list[3])
+		if not (os.path.exists(self.filepath)):
+			print("Cannot find file at path:", self.filepath)
+			return False
 		else:
-			stripped = re.sub("\s", "", line)
-			if stripped == "(m:s.ms)(mV)(mV)":
-				readingData = True
+			return True
+
+	# Opens the saved DataFrame
+	# Returns Df if successful, None otherwise
+	def getDfFromText(self):
+
+		fLen = len(self.filepath)
+
+		#Check that our filepath is a .txt
+		if (fLen > 4) and (self.filepath[fLen-4:] == ".txt"):
+
+			#open data for reading
+			data = open(self.filepath, "r")
+
+			readingData = False
+			col1, col2, col3 = []
+
+			for line in data.readlines():
+
+				if readingData:
+					data_list = line.split("\t")
+					# print(
+					# 	re.sub("\s", "", data_list[1]),
+					# 	re.sub("\s", "", data_list[2]),
+					# 	re.sub("\s", "", data_list[3]),
+					# 	)
+					col1.append(data_list[1])
+					col2.append(data_list[2])
+					col3.append(data_list[3])
+
+				#Ignore initial lines
+				else:
+					stripped = re.sub("\s", "", line)
+					#This line is the last line before data comes
+					if stripped == "(m:s.ms)(mV)(mV)":
+						readingData = True
 
 
-	data.close()
-else:
-	print("Cannot handle file", filepath)
-	exit()
-df = pd.DataFrame(list(zip(col1, col2, col3)), columns =['Time', 'Channel1', "Channel2"])
+			data.close()
+
+			df = pd.DataFrame(list(zip(col1, col2, col3)), columns =['Time', 'Channel1', "Channel2"])
+			# print(df)
+			return df
+
+		else:
+			print("Cannot handle file", self.filepath)
+			return None
+
+test = inputMassager()
+test.askForInput("Blah!")
+df = test.getDfFromText()
 print(df)
